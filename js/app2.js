466641ada27144bffde4a1387b2e07d63bf95abf
dkmullen <dkmullen@gmail.com>
2015-11-27 13:54:29 -0500

    Makes the game paint to screen, player moves at 10 px per keystroke

--- js/app2.js
+++ js/app2.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(sprite, x, y) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-	this.x = x;
-	this.y = y;
+	this.x = 0;
+	this.y = 20;
 };
 
 // Update the enemy's position, required method for game
@@ -26,59 +26,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-function Player(sprite, x, y) {
+var Player = function() {
 	this.sprite = 'images/char-boy.png';
-	this.x = 203;
-	this.y = 400;
+	this.x = 200;
+	this.y = 500;
 };
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-Player.prototype.handleInput = function(input, allowedKeys) {
-	if (input == 'up') {
-		/* if player completely reaches the water, sends him 
-		back to the bottom */
-		if (player.y == 0) {
-			player.y = 400;
-		}
-		else {
-			player.y -=10;
-		}		
-	}
-	else if (input == 'down') {
-		//keeps player from going below start point
-		if (player.y < 400) {
-			player.y +=10;
-		}
-	}
-	else if (input == 'left') {
-		// keeps player on the board
-		if (player.x >= 10) {
-			player.x -=10;
-		}
-	}
-	else if (input == 'right') {
-		if (player.x <= 400) {
-			player.x +=10;
-		}
-		
-	}
-};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy('images/enemy-bug.png', 10, 65);
-var enemy2 = new Enemy('images/enemy-bug.png', 200, 147);
-var enemy3 = new Enemy('images/enemy-bug.png', 100, 230);
-var allEnemies = [enemy1, enemy2, enemy3];
-var player = new Player();
-
+var player = function() {
+	Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+};
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+var allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
