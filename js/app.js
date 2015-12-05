2df876ea8348c8888f66204dd5f1b5682770b0c1
dkmullen <dkmullen@gmail.com>
2015-12-05 11:23:55 -0500

    Add high score, refine reset to resent tries and score, rudimentary choice of sprite

--- js/app.js
+++ js/app.js
@@ -22,11 +22,20 @@ var threeOptions = function() {
 };
 var tries = 3;
 var score = 0;
+var highScore = 0;
 var scoring = function() {
-	document.getElementById("score_box").innerHTML = "Score: " + score;
-	document.getElementById("tries_remaining").innerHTML = "Tries Remaining: " + tries;
+	document.getElementById("score_box").innerHTML = "Score: " + score + " ---";
+	document.getElementById("tries_remaining").innerHTML = "Tries Remaining: " + tries + " ---";
+	document.getElementById("high_score").innerHTML = "Your High Score: " + highScore;
 };
+//put the starting score of 0 and tries remaining on the screen as game starts
 scoring();
+document.getElementById("sprite1").addEventListener("click", chooseSprite);
+
+function chooseSprite() {
+	player.sprite = 'images/char-boy.png';
+};
+
 
 // Enemies our player must avoid
 var Enemy = function(sprite, x, y, speed) {
@@ -68,19 +77,28 @@ function Player(sprite, x, y) {
 	this.sprite = 'images/char-cat-girl.png';
 	this.x = startX;
 	this.y = startY;
+	//reset puts player back to the start, updates score and tries...
 	this.reset = function() {
 		player.x = startX;
 		player.y = startY;
-		scoring();
+		//...and checks to see if any tries are left
 		if (tries == 0) {
-			alert("Game Over! Score: " + score)
+			alert("Game Over! Score: " + score);
+			if (score > highScore) {
+				highScore = score;
+			}
+			score = 0;
+			tries = 3;
 		}
+		scoring();
 	};
 };
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	verticalMove = 83;
+	horizontalMove = 100;
 };
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -88,12 +106,10 @@ Player.prototype.render = function() {
 
 //The following controls the player's movement
 Player.prototype.handleInput = function(input, allowedKeys) {
-	verticalMove = 83;
-	horizontalMove = 100;
 	if (input == 'up') {
-		/* if player completely reaches the water, sends him 
-		back to the bottom */
 		player.y -=verticalMove;
+		/* if player completely reaches the water, add 200 to score, add a try,
+		send player back to start, update onscreen score/tries */
 		if (player.y <= water) {
 			score += 200;
 			tries += 1;
@@ -102,18 +118,19 @@ Player.prototype.handleInput = function(input, allowedKeys) {
 		}
 	}
 	else if (input == 'down') {
-		//keeps player from going below start point
+		//moves player down unless player is at bottomWall
 		if (player.y < bottomWall) {
 			player.y +=verticalMove;
 		}
 	}
 	else if (input == 'left') {
-		// keeps player on the board
+		// moves player left unless at leftWall
 		if (player.x > leftWall) {
 			player.x -=horizontalMove;
 		}
 	}
 	else if (input == 'right') {
+		//moves player right unless at rightWall
 		if (player.x < rightWall) {
 			player.x +=horizontalMove;
 		}
@@ -130,17 +147,27 @@ Treasure.prototype.render = function() {
 
 
 function checkCollisions() {
+	//check player-enemy colllisons
+	//loop through each enemy
 	for (i = 0; i < allEnemies.length; i++) {
+		//see if y values match exactly and x values are within 80 in either direction
+		//the value of 80 helps eliminate collisons of sprite margins
 		if (player.y == allEnemies[i].y && Math.abs(player.x - allEnemies[i].x) < 80) {
 			tries -= 1;
 			player.reset();
 		}
 	}
+	//check player-treasure collisons
+	//loop through each treasure
 	for (i = 0; i < allTreasures.length; i++) {
+		//see if x and y values match
 		if (player.x == allTreasures[i].x && player.y == allTreasures[i].y) {
+			//remove the treasures from the board's visible area
 			allTreasures[i].x = -300;
 			allTreasures[i].y = 396;
+			//award the player 100 points
 			score += 100;
+			//update the scoring displayed on screen
 			scoring();
 		}
 	}
@@ -152,27 +179,39 @@ function checkCollisions() {
 // Place the player object in a variable called player
 
 var allEnemies = [];
-for (i=0; i < 6; i++) {
-	//x[0] varies start point from just off the board to about halfway across the board...
-	//...and x[1] varies placement off (to the left) of the board
-	var x = [((Math.random() - 0.2) * 450), ((Math.random() * -500) - 200)];
-	//y values chosen to center enemies vertically on path
-	var y = [64, 147, 230];
-	//provides a variety of speeds
-	speed = ((Math.random() * 600) + 200);
-	//choose x[0] or x[1] and y[0], y[1] or y[2]
-	var n = new Enemy(this.sprite, x[twoOptions()], y[threeOptions()], speed);
-	//add n to the array and repeat the loop till there are six enemies
-	allEnemies.push(n);
+//loop thru this function pushing six enemies to allEnemies
+var newEnemy = function() {
+	for (i=0; i < 6; i++) {
+		//x[0] varies start point from just off the board to about halfway across the board...
+		//...and x[1] varies placement off (to the left) of the board
+		var x = [((Math.random() - 0.2) * 450), ((Math.random() * -500) - 200)];
+		//y values chosen to center enemies vertically on path
+		var y = [64, 147, 230];
+		//provides a variety of speeds
+		speed = ((Math.random() * 600) + 200);
+		//choose x[0] or x[1] and y[0], y[1] or y[2]
+		var n = new Enemy(this.sprite, x[twoOptions()], y[threeOptions()], speed);
+		allEnemies.push(n);			
+	}
 };
+newEnemy();
+
 var player = new Player();
 
 var allTreasures = [];
-for (i = 0; i <= (threeOptions()); i++) {
-	var treasure = new Treasure(treasureSprite[threeOptions()], 
-	   treasureX[threeOptions()], treasureY[twoOptions()]);
-	allTreasures.push(treasure);
+//loop through this function and push one, two or three treasures to allTreasures
+var newTreasure = function() {
+	//chose randomly among the three treasure sprites and place them randomly
+	//in the six squares of the treasure area (problem - can put two or three in one square)
+	for (i = 0; i <= (threeOptions()); i++) {
+		var treasure = new Treasure(treasureSprite[threeOptions()], 
+		   treasureX[threeOptions()], treasureY[twoOptions()]);
+		allTreasures.push(treasure);
+			
+	}
 };
+newTreasure();
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
--- js/engine.js
+++ js/engine.js
@@ -178,6 +178,10 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-cat-girl.png',
+		'images/char-boy.png',
+		'images/char-horn-girl.png',
+		'images/char-pink-girl.png',
+		'images/char-princess-girl.png',
 		'images/Key.png',
 		'images/Star.png',
 		'images/Gem Blue.png'
